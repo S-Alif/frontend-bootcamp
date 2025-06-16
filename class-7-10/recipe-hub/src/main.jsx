a7832dc055b52a2da7c217b7b73866b0007cf24b
s-alif <alif160038@gmail.com>
2025-06-16 21:52:12 +0600

    DONE - home page: home, about section

--- class-7-10/recipe-hub/src/main.jsx
+++ class-7-10/recipe-hub/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+// route object
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />
+	},
+])
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
 )
